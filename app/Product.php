5c1fd5cececc839f0e22c6afad9b4cd7c76a3e77
Emeka Mbachu <victor.c.mbachu@gmail.com>
2020-06-30 01:53:19 +0100

    First Commit

---
+++ app/Product.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    public const AVAILABLE_PRODUCT = 'available';
+    public const UNAVAILABLE_PRODUCT = 'unavailable';
+
+    protected $fillable = [
+
+        'name',
+        'description',
+        'quantity',
+        'status',
+        'image',
+        'seller_id',
+
+    ];
+
+    public function isAvailable(){
+        return $this->status === self::AVAILABLE_PRODUCT;
+    }
+
+    public function categories(){
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function transactions(){
+        return $this->hasMany(Transaction::class);
+    }
+
+    public function seller(){
+        return $this->belongsTo(Seller::class);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+//Buyers
+Route::resource('buyers', 'BuyerController', ['only' => ['index', 'show']]);
+
+//Categories
+Route::resource('categories', 'CategoryController', ['except' => ['create', 'edit']]);
+
+//Products
+Route::resource('products', 'ProductController', ['only' => ['index', 'show']]);
+
+//Sellers
+Route::resource('sellers', 'SellerController', ['only' => ['index', 'show']]);
+
+//Transactions
+Route::resource('transactions', 'TransactionController', ['only' => ['index', 'show']]);
+
+//Users
+Route::resource('users', 'UserController', ['except' => ['create', 'edit']]);
